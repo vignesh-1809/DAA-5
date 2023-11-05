d8ebba6e3620dbc3f2e8eb48cefca7618ddd9650
vignesh-1809 <149823783+vignesh-1809@users.noreply.github.com>
2023-11-06 00:08:32 +0530

    Create q4.c

---
+++ Practice 25-09/q4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+void greedySort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex != i) {
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+}
+int main() {
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    greedySort(arr, n);
+
+    printf("\nSorted Array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
