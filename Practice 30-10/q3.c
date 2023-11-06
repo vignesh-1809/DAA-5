fc78743fbf8dac91c01532f471e3810274aa68a0
vignesh-1809 <149823783+vignesh-1809@users.noreply.github.com>
2023-11-06 16:14:17 +0530

    Create q3.c

---
+++ Practice 30-10/q3.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <limits.h>
+
+#define V 6 // Number of vertices in the graph
+
+int minDistance(int dist[], bool sptSet[]) {
+    int min = INT_MAX, min_index;
+
+    for (int v = 0; v < V; v++) {
+        if (sptSet[v] == false && dist[v] < min) {
+            min = dist[v];
+            min_index = v;
+        }
+    }
+
+    return min_index;
+}
+
+void printSolution(int dist[]) {
+    printf("Vertex \t Distance from Source\n");
+    for (int i = 0; i < V; i++) {
+        printf("%d \t %d\n", i, dist[i]);
+    }
+}
+
+void dijkstra(int graph[V][V], int src) {
+    int dist[V];
+    bool sptSet[V];
+
+    for (int i = 0; i < V; i++) {
+        dist[i] = INT_MAX;
+        sptSet[i] = false;
+    }
+
+    dist[src] = 0;
+
+    for (int count = 0; count < V - 1; count++) {
+        int u = minDistance(dist, sptSet);
+        sptSet[u] = true;
+
+        for (int v = 0; v < V; v++) {
+            if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX &&
+                dist[u] + graph[u][v] < dist[v]) {
+                dist[v] = dist[u] + graph[u][v];
+            }
+        }
+    }
+
+    printSolution(dist);
+}
+
+int main() {
+    int graph[V][V] = {{0, 2, 0, 1, 0, 0},
+                       {2, 0, 4, 0, 3, 0},
+                       {0, 4, 0, 5, 0, 2},
+                       {1, 0, 5, 0, 4, 3},
+                       {0, 3, 0, 4, 0, 1},
+                       {0, 0, 2, 3, 1, 0}};
+
+    dijkstra(graph, 0);
+
+    return 0;
+}
